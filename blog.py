de9145e8facb7aaf3c430ca273c1d41f1006601d
Arif Kalaycı <201118050@samsun.edu.tr>
2022-02-05 13:41:31 +0300

    blog

---
+++ blog.py
@@ -0,0 +1,169 @@
+from flask import Flask,render_template,flash,redirect,url_for,session,logging,request
+from flask_mysqldb import MySQL
+from wtforms import Form,StringField,TextAreaField,PasswordField,validators
+from passlib.hash import sha256_crypt
+from functools import wraps
+
+#kullanıcı giriş decorater(https://flask.palletsprojects.com/en/2.0.x/patterns/viewdecorators/)
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if "logged_in" in session:#kullanıcı giriş yapmış mı yapmışsa True döner
+            return f(*args, **kwargs)
+        else:
+            flash("bu sayfayı görüntülemek için lütfen giriş yapınız...","danger")
+            return redirect(url_for("login"))
+    return decorated_function
+
+#kullanıcı kayıt formu
+class RegisterForm(Form):
+    name=StringField("İsim Soyisim",validators=[validators.Length(min=4,max=10)])
+    username=StringField("Kullanıcı Adı",validators=[validators.Length(min=4,max=30)])
+    email=StringField("e-posta",validators=[validators.Email(message="lütfen geçerli bir email giriniz")])
+    password=PasswordField("Parola:",validators=[
+        validators.DataRequired(message="lütfen geçerli bir parola giriniz"),
+        validators.EqualTo(fieldname="confirm",message="parolanız uyuşmuyor")
+        ])
+    confirm=PasswordField("parola doğrula")
+
+#giriş
+class LoginForm(Form):
+    username=StringField("Kullanıcı Adı")
+    password=PasswordField("Parola")
+
+app=Flask(__name__)
+
+app.config["MYSQL_HOST"]="localhost"
+app.config["MYSQL_USER"]="root"
+app.config["MYSQL_PASSWORD"]=""
+app.config["MYSQL_DB"]="blog"
+app.config["MYSQL_CURSORCLASS"]="DictCursor"
+
+mysql=MySQL(app)
+app.secret_key="blog" #flashing için gerekli
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/articles")
+def articles():
+    cursor=mysql.connection.cursor()
+
+    sorgu="Select * From articles"
+    result=cursor.execute(sorgu)
+    if result>0:
+        articles=cursor.fetchall()
+        return render_template("articles.html",articles=articles)
+    else:
+        return render_template("articles.html")
+
+
+#kontrol paneli
+@app.route("/dashboard")
+@login_required #yalnızca giriş yapıldığında görüntülenebilir.
+def dashboard():
+    cursor=mysql.connection.cursor()
+    sorgu="Select * From articles where author=%s"
+    result=cursor.execute(sorgu,(session["username"],))
+
+    if result>0:
+        return render_template("dashboard.html",articles=articles)
+    else:
+        return render_template("dashboard.html")
+
+
+#kayıt olma
+@app.route("/register",methods=["GET","POST"])
+def register():
+    form=RegisterForm(request.form)
+
+    if request.method=="POST" and form.validate():
+        name=form.name.data
+        username=form.name.data
+        email=form.email.data
+        password=sha256_crypt.encrypt(form.password.data)
+
+        cursor=mysql.connection.cursor()
+        sorgu="Insert into users(name,username,email,password) VALUES(%s,%s,%s,%s)"
+        cursor.execute(sorgu,(name,username,email,password))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("basarıyla kayıt oldunuz.","success")
+        return redirect(url_for("index"))
+    else:
+        return render_template("register.html",form=form)
+#giriş yap
+@app.route("/login",methods=["GET","POST"])
+def login():
+    form=LoginForm(request.form)
+    if request.method=="POST":
+        username=form.username.data
+        password_entered=form.password.data
+
+        cursor=mysql.connection.cursor()
+        sorgu="Select * From users where username =%s"
+        result=cursor.execute(sorgu,(username,))
+        
+        if result>0:
+            data=cursor.fetchone()#veritabanındaki bilgilri alır
+            real_password=data["password"]
+            if sha256_crypt.verify(password_entered,real_password):#karşılaştırma fonksiyonu
+                flash("Başarıyla giriş yaptınız...","success")
+
+                session["logged_in"]=True
+                session["username"]=username
+
+                return redirect(url_for("index"))
+        else:
+            flash("böyle bir kullanıcı bulunmuyor","danger")
+            return redirect(url_for("login"))
+
+    return render_template("login.html",form=form)
+#çıkış yapma
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+#makale ekleme
+@app.route("/addarticle",methods=["GET","POST"])
+def addarticle():
+    form=ArticleForm(request.form)
+    if request.method=="POST":
+        title=form.title.data
+        content=form.content.data
+
+        cursor=mysql.connection.cursor()
+
+        sorgu="Insert into articles(title,author,content) VALUES(%s,%s,%s)"
+        cursor.execute(sorgu,(title,session["username"],content))
+
+        mysql.connection.commit()
+        cursor.close()
+
+        flash("Makale başarıyla eklendi...","success")
+        return redirect(url_for("dashboard"))
+
+
+    return render_template("addarticle.html",form=form)
+#makale form
+class ArticleForm(Form):
+
+    title=StringField("Makale Başlığı")
+    content=TextAreaField("Makale İçeriği")
+    
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
